c99ef5c518145f3eb8dfbd7db4246f0454b4b044
Daniel Sun <=>
2017-12-15 16:29:39 -0800

    left align header text and hide bottom bar

--- FSCalendar/FSCalendarStickyHeader.m
+++ FSCalendar/FSCalendarStickyHeader.m
@@ -37,7 +37,7 @@
         self.contentView = view;
         
         label = [[UILabel alloc] initWithFrame:CGRectZero];
-        label.textAlignment = NSTextAlignmentCenter;
+        label.textAlignment = NSTextAlignmentLeft;
         label.numberOfLines = 0;
         [_contentView addSubview:label];
         self.titleLabel = label;
@@ -46,6 +46,7 @@
         view.backgroundColor = FSCalendarStandardLineColor;
         [_contentView addSubview:view];
         self.bottomBorder = view;
+        self.bottomBorder.hidden = YES;
         
         FSCalendarWeekdayView *weekdayView = [[FSCalendarWeekdayView alloc] init];
         [self.contentView addSubview:weekdayView];
@@ -69,7 +70,7 @@
     CGFloat titleHeight = [@"1" sizeWithAttributes:@{NSFontAttributeName:self.calendar.appearance.headerTitleFont}].height*1.5 + weekdayMargin*3;
     
     _bottomBorder.frame = CGRectMake(0, _contentView.fs_height-weekdayHeight-weekdayMargin*2, _contentView.fs_width, 1.0);
-    _titleLabel.frame = CGRectMake(0, _bottomBorder.fs_bottom-titleHeight-weekdayMargin, titleWidth,titleHeight);
+    _titleLabel.frame = CGRectMake(16, _bottomBorder.fs_bottom-titleHeight-weekdayMargin, titleWidth-16,titleHeight);
     
 }
 
